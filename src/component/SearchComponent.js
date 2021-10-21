2369d404dc31e62a61a4c58ce0dd9deb58afa8ad
peeyushkumar22 <88648852+peeyushkumar22@users.noreply.github.com>
2021-10-21 09:23:49 +0530

    github users by search

--- src/component/SearchComponent.js
+++ src/component/SearchComponent.js
@@ -3,96 +3,56 @@ import { useState } from "react";
 import { useHistory } from "react-router";
 import { useEffect } from "react/cjs/react.development";
 import { Form } from "semantic-ui-react";
-import axios from "axios";
-
 
 const SearchComponent = () => {
-  // const suggestions=[
-  //   "shyamsha",
-  //   "subahm-jain",
-  //   "Peeyush-kumar7656",
-  //   "a4aakash",
-  //   "a4akash",
-  //   "deven",
-  //   "subhajit"
-  // ]
-  const [userInput, setUserInput] = useState(" ");
+  const [userInput, setUserInput] = useState("");
   const [error, setError] = useState(null);
-  const [githubUsers,setGithubusers]=useState([]);
-  // const [filteredUsers, setFilteredUsers] = useState([]);
-  // const[display,setDisplay]=useState(false);
-  // const [options,setOptions]=useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [githubUsersAll, setGithubUsersAll] = useState([]);
 
-  // useEffect(()=>{
-  //   const githubUsers=[];
-  //   const promises=new Array.fill().map((v)=>fetch(`https://api.github.com/users`));
-  //   Promise.all(promises).then(githubUsersArr => {
-  //     return githubUsersArr.map(value =>
-  //       value
-  //         .json()
-  //         .then(({ login }) =>
-  //           githubUsers.push({login})
-  //         )
-  //     );
-  //   });
-  //   setOptions(githubUsers);
-  //   console.log(githubUsers);
-  // }, [userInput]);
-
-  // useEffect(() => {
-  //   axios
-  //     .get("https://api.github.com/users")
-  //     .then(res => {
-  //       setGithubusers(res.data);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // }, []);
-
-  // useEffect(() => {
-  //   setFilteredUsers(
-  //     githubUsers.filter(users =>{
-  //      return users.login.toLowerCase().includes(userInput.toLowerCase())
-  //     })
-  //   );
-  // }, [userInput,githubUsers]);
-
- 
-  useEffect(() => {
-    const loadGithubUsers=async ()=>{
-      const response= await axios.get("https://api.github.com/users")
-          setGithubusers(response.data);
-        };
-        loadGithubUsers();
-      },[]);
-
-      console.log(githubUsers);
+  async function loadGithubAllUsers() {
+    const response = await fetch(`https://api.github.com/users`);
+    const data = await response.json();
+    console.log(data);
+    if (data) {
+      const users = data.map((eachUser) => {
+        return eachUser.login;
+      });
+      setGithubUsersAll(users);
+    }
+  }
 
   const history = useHistory();
   const handleSearch = (e) => {
     setUserInput(e.target.value);
   };
-  // const handleSearch=(userInput)=>{
-  //    let matches = githubUsers.filter((users)=>{
-  //       const regex=new RegExp(`${userInput}`,"gi")
-  //       return users.login.match(regex);
-  //    })
-  //     setGithubusers(matches);
-  // }
+
+  useEffect(() => {
+    setFilteredUsers(
+      githubUsersAll.filter((eachUser) => {
+        return eachUser.toLowerCase().includes(userInput.toLowerCase());
+      })
+    );
+  }, [userInput]);
 
   const handleSubmit = () => {
-          if(userInput.length>0){
-          history.push({
-            search:"?" + new URLSearchParams({userInput:userInput}).toString(),
-            pathname: '/userProfile',
-            state: { userInput:userInput }
-          });
-          
-          }
-          else{
-            setError("Please type correct name");
-          }
+    if (userInput.length > 0) {
+      history.push({
+        search: "?" + new URLSearchParams({ userInput: userInput }).toString(),
+        pathname: "/userProfile",
+        state: { userInput: userInput },
+      });
+    } else {
+      setError("Please type correct name");
+    }
+  };
+
+  useEffect(() => {
+    loadGithubAllUsers();
+  }, []);
+
+  const selectUser = (user) => {
+    setUserInput(user);
   };
   return (
     <>
@@ -101,51 +61,33 @@ const SearchComponent = () => {
         <Form onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Input
+              autoComplete="off"
               placeholder="Github user"
               name="github user"
               onChange={handleSearch}
-              // onClick={()=>setDisplay(!display)}
-              // onChange={(e)=>handleSearch(e.target.value)}
+              value={userInput}
             />
-            <Form.Button content="Search"/>
+
+            <Form.Button content="Search" />
           </Form.Group>
         </Form>
       </div>
-      {error ? <h1>Please type correct name</h1> : null}
-      {/* <Autocomplete suggestions={suggestions}/> */}
-      {/* {display && (
-        <div className="autoContainer">
-          {options
-            .filter(({login }) => login.value(userInput))
-            .map((value) => {
-              return (
-                  <span>{value.login}</span>
-              );
-            })} */}
-        {/* </div>
-      )} */}
-
-      {/* {filteredUsers.map((users, idx) => (
-        <GithubDetail key={idx} {...users} />
-      ))}
-      {filteredUsers.length === 0 && <p>No users found</p>} */}
-
-      {/* {githubUsers && githubUsers.map((item,index) => {
-          <div key={index} style={{marginLeft:"35%",marginTop:"10px"}}>
-            <Card style={{width:'50%'}} title={`${item.login}`}></Card>
-          </div> */}
-      {/* })} */}
+      {userInput &&
+        filteredUsers &&
+        filteredUsers.map((user) => {
+          return (
+            <ul style={{ marginLeft: "35%", marginTop: "10px" }}>
+              <li
+                style={{ width: "50%", cursor: "pointer" }}
+                onClick={() => selectUser(user)}
+              >
+                {user}
+              </li>
+            </ul>
+          );
+        })}
+      {!userInput && <h1 style={{marginLeft:"39.5%",marginTop:"2%",color:"red"}}>{error}</h1>}
     </>
   );
-}
-
-// const GithubDetail = props => {
-//   const {login} = props;
-//   return (
-//     <>
-//       <p>{login}</p>
-//     </>
-//   );
-// };
-
+};
 export default SearchComponent;
--- src/component/UserProfile.js
+++ src/component/UserProfile.js
@@ -1,83 +1,99 @@
-import React from 'react'
-import { useState} from 'react';
-import { useHistory } from 'react-router';
-import { useEffect } from 'react/cjs/react.development';
-import {Card,Image,Icon} from "semantic-ui-react";
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import { useEffect } from "react/cjs/react.development";
+import { Card, Image, Icon } from "semantic-ui-react";
 const UserProfile = () => {
   // const{userInput}=props;
-  const history=useHistory();
-    const [name,setName]=useState('');
-    const [userName,setUsername]=useState('');
-    const [bio,setBio]=useState('');
-    const[id,setId]=useState('');
-    const[company,setCompany]=useState('');
-    const[location,setLocation]=useState('');
-    const [followers,setFollowers]=useState('');
-    const [following,setFollowing]=useState('');
-    const [repos,setRepos]=useState('');
-    const [avatar,setAvatar]=useState('');
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [userName, setUsername] = useState("");
+  const [bio, setBio] = useState("");
+  const [id, setId] = useState("");
+  const [company, setCompany] = useState("");
+  const [location, setLocation] = useState("");
+  const [followers, setFollowers] = useState("");
+  const [following, setFollowing] = useState("");
+  const [repos, setRepos] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [totalData, setTotalData] = useState({});
 
-    const setData = ({name,id,company,location,login,followers,following,public_repos,bio,avatar_url}) => {
-        setName(name);
-        setUsername(login);
-        setId(id);
-        setLocation(location);
-        setCompany(company);
-        setFollowers(followers);
-        setFollowing(following);
-        setRepos(public_repos);
-        setAvatar(avatar_url);
-        setBio(bio);
-    
-      };
+  const setData = ({
+    name,
+    id,
+    company,
+    location,
+    login,
+    followers,
+    following,
+    public_repos,
+    bio,
+    avatar_url,
+  }) => {
+    setName(name);
+    setUsername(login);
+    setId(id);
+    setLocation(location);
+    setCompany(company);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+    setBio(bio);
+  };
 
-       useEffect(()=>{
-        fetch(`https://api.github.com/users/${history.location.state.userInput}`)
-        .then(res=>res.json())
-        .then(data=>{
-          setData(data);
-        });
-      },[history.location.state.userInput]);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${history.location.state.userInput}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setTotalData(data);
+        console.log(data);
+      });
+  }, [history.location.state.userInput]);
 
-      console.log(history.location.state.userInput);
-    return (
+  return (
+    <>
+      {totalData.login && (
         <div className="card">
-        <Card>
-    <Image src={avatar} wrapped ui={false} />
-    <Card.Content>
+          <Card>
+            <Image src={avatar} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{name}</Card.Header>
+              <Card.Header>{userName}</Card.Header>
+              <Card.Header>{id}</Card.Header>
+              <Card.Header>{location}</Card.Header>
+              <Card.Header>{company}</Card.Header>
 
-      <Card.Header>{name}</Card.Header>
-      <Card.Header>{userName}</Card.Header>
-      <Card.Header>{id}</Card.Header>
-      <Card.Header>{location}</Card.Header>
-      <Card.Header>{company}</Card.Header>
+              <Card.Meta>
+                <span>{repos}</span>
+              </Card.Meta>
 
+              <Card.Description>{bio}</Card.Description>
+            </Card.Content>
 
-      <Card.Meta>
-        <span>{repos}</span>
-      </Card.Meta>
-
-      <Card.Description>
-        {bio}
-      </Card.Description>
-
-    </Card.Content>
-
-    <Card.Content extra> 
-      <a href>
-        <Icon name='user' />
-        {followers} followers
-      </a>
-    </Card.Content>
-    <Card.Content extra> 
-    <a href>
-        <Icon name='user' />
-        {following} following
-      </a>
-    </Card.Content>
-  </Card>
+            <Card.Content extra>
+              <a
+                href={`https://api.github.com/users/${history.location.state.userInput}/followers`}
+              >
+                <Icon name="user" />
+                {followers} followers
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a
+                href={`https://api.github.com/users/${history.location.state.userInput}/following`}
+              >
+                <Icon name="user" />
+                {following} following
+              </a>
+            </Card.Content>
+          </Card>
         </div>
-    )
-}
+      )}
+      {!totalData.login && <h1 style={{marginLeft:"39.5%",marginTop:"2%",color:"red"}}>User not found</h1>}
+    </>
+  );
+};
 
-export default UserProfile
+export default UserProfile;
